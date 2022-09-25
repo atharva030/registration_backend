c9e3d8152bf851908091f73eca3854238d1741ac
atharva030 <91479983+atharva030@users.noreply.github.com>
2022-09-26 00:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ContentModel = require("./models/Content");
+const ContentModel = require("./Models/Content");
 const mongoose = require("mongoose");
 const app=express();
 const port=8000;
