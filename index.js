e4b28315865f005c557316f983f230809e48f96f
atharva_gitlab_030 <atharvas_jagdale@auton.in>
2022-11-17 19:53:56 +0530

    rnxg-backend

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-require('dotenv').config()
 const express = require("express");
 const ContentModel = require("./Models/Content");
 const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://atharva:Pankhuri@cluster0.3asiwc6.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+var cors = require("cors");
 
-const app=express();
-const port=8000;
-const mongoURL=process.env.MONGO_URL
-var cors=require('cors')
-let PORT = process.env.PORT;
-
-app.use(express.json());
-
+const port = 5000;
 const connectionParams={
     useNewUrlParser:true,
     useUnifiedTopology:true
-}
-app.use(cors())
-app.post("/registration",async(req,res)=>{
-   
-  
-    const data = new ContentModel({
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          email: req.body.email,
-          phone:req.body.phone,
-           org_name: req.body.org_name,
-           org_address: req.body.org_address,
-           password: req.body.password,
-  })
+  }
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/registration", async (req, res) => {
+  const data = new ContentModel({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    phone: req.body.phone,
+    org_name: req.body.org_name,
+    org_address: req.body.org_address,
+    password: req.body.password,
+  });
 
   try {
-      const dataToSave = await data.save();
-      res.status(200).json(dataToSave)
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-  catch (error) {
-      res.status(400).json({message: error.message})
-  }
-
-})
- mongoose.connect(mongoURL,connectionParams).then(()=>{
-    console.info("connected")
-  }).catch((error)=>{
-      console.log("Error: ",error)
+});
+mongoose
+  .connect(mongoURI, connectionParams)
+  .then(() => {
+    console.info("connected");
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
   });
-app.listen(PORT || 5000, () => {
-   // console.log(`Example app listening on port ${port}`);
-  });
-  
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
